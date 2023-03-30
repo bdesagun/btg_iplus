31081d32e6ab1c8f1fcd295eec01841b042a90a4
DAVE TABLANTE <dave.tablante@wdc.com>
2023-03-30 19:26:56 +0800

    changing email credential to btgi

--- application/config/email.php
+++ application/config/email.php
@@ -3,12 +3,10 @@
 $config = array(
     'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
     'smtp_host' => 'smtp.outlook.office365.com',
-    'smtp_port' => 465,
-    'smtp_user' => 'dave.tablante0315@gmail.com',
-    'smtp_pass' => 'exznzajsxjgtlesb',
-    // 'smtp_user' => 'dave.tablante@btgi.com.au',
-    // 'smtp_pass' => 'Asd@Qwe#',
-    'smtp_crypto' => 'ssl', //can be 'ssl' or 'tls' for example
+    'smtp_port' => 587,
+    'smtp_user' => 'dave.tablante@btgi.com.au',
+    'smtp_pass' => 'Asd@Qwe#',
+    'smtp_crypto' => 'tls', //can be 'ssl' or 'tls' for example
     'mailtype' => 'html', //plaintext 'text' mails or 'html'
     'smtp_timeout' => '30', //in seconds
     'charset' => 'iso-8859-1',
